00b22503e35de163ac53d8cae28d6efbfe0b1067
Gabriel Santos <gabriiel.slimaa@gmail.com>
2023-05-08 19:18:06 -0300

    Modificando o database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,12 @@
 module.exports = {
-    dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: '',
-    database: 'crud-node',
+    dialect: "mysql",
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "cqQt9zfsTHR06IANS4QG",
+    database: process.env.DB_DBNAME || "railway",
+    host: process.env.DB_HOST || "containers-us-west-98.railway.app",
+    port:process.env.DB_PORT || "6902",
     define: {
-        timestamp: true,
-        underscored: true
+      timestamp: true,
+      underscored:true
     }
-}
+  }
