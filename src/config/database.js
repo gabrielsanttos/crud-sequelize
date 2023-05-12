60ae914c463116f8221cdfaf361bd6fd2ea8bc95
Gabriel Santos <gabriiel.slimaa@gmail.com>
2023-05-11 22:59:31 -0300

    Corrigindo o arquivo database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
     dialect: "mysql",
-    username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "cqQt9zfsTHR06IANS4QG",
-    database: process.env.DB_DBNAME || "railway",
-    host: process.env.DB_HOST || "containers-us-west-98.railway.app",
-    port:process.env.DB_PORT || "6902",
+    username: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "cqQt9zfsTHR06IANS4QG",
+    database: process.env.MYSQLDATABASE || "railway",
+    host: process.env.MYSQLHOST || "containers-us-west-98.railway.app",
+    port:process.env.MYSQLPORT || "6902",
     define: {
       timestamp: true,
       underscored:true
